4cd4a1959a1f13443509123269469506add66114
Baokhoa Vu <tealkrysta92@gmail.com>
2018-06-29 05:01:42 -0700

    current front page

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -20,15 +20,34 @@ export default class Home extends Component {
     render() {
       return (
             <article>
-                {this.renderBreadcrumbs()}
-                <h1>ReactJs, Node and Bootstrap boilerplate</h1>
-                <p className="lead">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel odio ac felis consectetur commodo. Duis ac ligula et nisl aliquam hendrerit. Morbi purus lectus, malesuada vel tristique at, gravida id felis. Integer dapibus suscipit neque nec ultricies. Sed sit amet vehicula risus. Suspendisse potenti. Pellentesque fringilla diam quis condimentum maximus. Sed eleifend neque eget justo rhoncus luctus. Donec sed luctus metus. Vestibulum vulputate accumsan elit, ac cursus nibh rutrum venenatis. In porttitor elementum condimentum. Maecenas porttitor est ac aliquam congue.
-                    <br /><br />
-                    Integer vitae euismod risus, sit amet tincidunt est. Mauris aliquam et lorem sit amet laoreet. Phasellus elementum vestibulum libero in consectetur. Suspendisse ullamcorper facilisis congue. Donec eu mi purus. Etiam dictum sed arcu non feugiat. Sed sed tempor augue. Morbi et lectus quis elit porta rutrum a at magna. Suspendisse volutpat id ipsum sed consequat. In egestas tempor urna, at finibus velit fringilla eget. Aliquam eleifend, libero ut tincidunt tristique, nunc arcu faucibus velit, eget rutrum risus odio ac turpis. Morbi id ornare nulla. Integer eleifend euismod leo in pharetra. Vivamus tristique leo quis dolor scelerisque, a ultrices est consequat. Aenean eget purus lectus. Praesent non ante placerat, auctor leo vel, ultricies justo.
-                </p>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm">
+                            <div class="blue-box">Who We Are</div>
+                            <div class="grey-box">
+                                <p>The HomeRatesToday.com Team is here to help you with questions regarding any information you have about our website. We pride ourselves in connecting you with lenders offering some of the lowests rates nationwide and make the loan process simple, straightfoward and fast for borrowers seeking a mortgage in the California and Washington area.
+                                </p>
+                                <ul class="body two">
+                                    <li>Accurate and reliable quotes</li>
+                                    <li>Quick 30 second form</li>
+                                    <li>No SSN required</li>
+                                    <li>No credit check conducted</li>
+                                    <li>Your information is kept private</li>
+                                    <li>Consultation with no obligation</li>
+                                    <li>No Hassle</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-sm">
+                            Hi
+                        </div>
+                        <div class="col-sm">
+                            Hi
+                        </div>
+                    </div>
+                </div>
                 <section className="row col-lg-12 nopadding">
-                    
+
                 </section>
             </article>
       );
